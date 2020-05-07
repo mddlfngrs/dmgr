161cb780e7b05ea05705428ab1efa4e78913b7c4
Jannik <widmann.jannik@gmail.com>
2020-05-07 09:37:03 +0200

    added all possible actions but with placeholders

--- dmgr.py
+++ dmgr.py
@@ -3,15 +3,29 @@ import getopt, sys
 
 class Action():
     def option(self, args):
-        print(str(args))
         if args in ("-h", "--help"):
-            print("h")
+             f = open("dat/helpfile.txt", "r")
+             print(f.read())
+             f.close()
         elif args in ("-v", "--version"):
-            #act1.option()
-            print("v")
+            print("dmgr v0.1 pre-alpha")
+        elif args in ("-i", "--install"):
+            print("placeholder for install")
+        elif args in ("-s", "--size"):
+            print("placeholder for size")
+        elif args in ("-f", "--free"):
+            print("placeholder for free")
+        elif args in ("-p", "--partition"):
+            print("placeholder for partition")
+        elif args in ("-c", "--copy"):
+            print("placeholder for copy")
+        elif args in ("-d", "--delete"):
+            print("placeholder for delete")
+        elif args in ("-x", "--cut"):
+            print("placeholder for cut")
         else:
             print("option " + args + " not recognized\nTry 'dmgr -h' for more information")
-            sys.exit()
+        sys.exit()
 
 class Option():
     def main(self):
