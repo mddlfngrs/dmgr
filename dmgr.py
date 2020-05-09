23ce054b353bb2d9b58b8f917db53991d71061b5
Jannik <widmann.jannik@gmail.com>
2020-05-09 15:28:56 +0200

    overall updates

--- dmgr.py
+++ dmgr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import getopt, sys
+import getopt, sys, subprocess as sp
+from pathlip import Path
 
 class Action():
     def option(self, args):
@@ -10,19 +11,27 @@ class Action():
         elif args in ("-v", "--version"):
             print("dmgr v0.1 pre-alpha")
         elif args in ("-i", "--install"):
+            if == 'deb':
+                sp.call(["sudo", "dpkg", "-i", src_file])
+            elif == 'rpm':
+                print("if this won't work, try\n\nsudo apt install rpm\n\nand try it again then")
+                sp.call(["sudo", "rpm", "-i", src_file])
             print("placeholder for install")
         elif args in ("-s", "--size"):
-            print("placeholder for size")
+            print("The file size is: " + str(Path(src_file).stat().st_size))
         elif args in ("-f", "--free"):
             print("placeholder for free")
         elif args in ("-p", "--partition"):
             print("placeholder for partition")
         elif args in ("-c", "--copy"):
-            print("placeholder for copy")
+            sp.call(["sudo", "cp", src_file, dest_file])
+            print("Already knew that the default command for that is:\ncp [src_file] [dest_file]")
         elif args in ("-d", "--delete"):
-            print("placeholder for delete")
+            sp.call(["sudo", "rm", "-rf", src_file])
+            print("Already knew that the default command for that is:\nrm -rf [src_file]")
         elif args in ("-x", "--cut"):
             print("placeholder for cut")
+            print("Already knew that the default command for that is:\ncp [src_file] [dest_file] && rm [src_file]")
         else:
             print("option " + args + " not recognized\nTry 'dmgr -h' for more information")
         sys.exit()
